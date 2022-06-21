d1106dbeb966a3225018c8e6fdecdae4d7091d2d
Ömer Salih Sülün <omersalih.sulun@gmail.com>
2022-06-21 15:06:16 +0300

    Remove old directory

--- firstcproject/controllers/c_controller/build/release/c_controller.d
+++
@@ -1,6 +0,0 @@
-build/release/c_controller.o: c_controller.c \
- C:\Users\omer-\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\omer-\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\omer-\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\omer-\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
- C:\Users\omer-\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h
